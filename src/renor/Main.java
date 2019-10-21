e2da3bb676c136a871eb1af8d77839bc8a81280e
Jared Diehl <jareddiehl96@gmail.com>
2019-10-20 21:27:45 -0500

    Updated Main.java

--- src/renor/Main.java
+++ src/renor/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
 
-		Renor renor = new Renor(new Session("Voidx", "69"), 854, 480, false, null, Proxy.NO_PROXY, "Indev");
+		Renor renor = new Renor(new Session("Jaredible", "3"), 854, 480, false, null, Proxy.NO_PROXY, "Indev");
 
 		Thread.currentThread().setName("Renor main thread");
 		renor.run();
